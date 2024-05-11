b1315343392490604cccd30a3dfe82255bf28214
HP-PROBOOK <HP-PROBOOK@CelesteFicoseco>
2024-05-11 20:30:52 -0300

    primer commit

---
+++ tp3_FicosecoCeleste/src/main/java/ar/edu/unju/fi/Tp3FicosecoCelesteApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3FicosecoCelesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3FicosecoCelesteApplication.class, args);
+	}
+
+}
